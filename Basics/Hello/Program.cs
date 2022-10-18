3dcd10153b9e9c50bbbd283755daaa3cadb40e2e
Vlad Moisak <pain1797@icloud.com>
2022-10-18 20:10:58 +0300

    Реализация: Hello.

---
+++ Basics/Hello/Program.cs
@@ -0,0 +1,11 @@
+﻿public static class Program
+{
+	public static void Main()
+	{
+		Console.Write("Как тебя зовут? ");
+		string name = Console.ReadLine();
+		Console.Write($"Привет, {name}!");
+		
+		Console.ReadLine();
+	}
+}
