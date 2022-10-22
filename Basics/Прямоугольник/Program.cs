047a52cb0b765d739405f22aa7e284a5b4fc774c
Vlad Moisak <pain1797@icloud.com>
2022-10-22 11:09:03 +0300

    Реализация: Rectangle.

---
+++ Basics/Прямоугольник/Program.cs
@@ -0,0 +1,20 @@
+﻿public static class Program
+{
+    public static void Main()
+    {
+        Console.Write("Задайте ширину прямоугольника: ");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.Write("Задайте высоту прямоугольника: ");
+        int b = int.Parse(Console.ReadLine());
+        for (int height = 1; height <= b; height++)
+        {
+            for (int width = 1; width <= a; width++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+        Console.ReadLine();       
+    }
+}
