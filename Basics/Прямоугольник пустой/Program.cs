cbc0bd46d2fd8661714c946be701bff382efbd2c
Tim Ivashchenko <tymofii.ivashchenko@gmail.com>
2022-10-24 16:22:00 +0300

    Косметика: пустой прямоугольник.

--- Basics/Прямоугольник пустой/Program.cs
+++ Basics/Прямоугольник пустой/Program.cs
@@ -1,28 +1,30 @@
 ﻿public static class Program
 {
-    public static void Main()
-    {
-        Console.Write("Задайте ширину прямоугольника: ");
-        int a = int.Parse(Console.ReadLine());
+	public static void Main()
+	{
+		Console.Write("Задайте ширину прямоугольника: ");
+		var a = int.Parse(Console.ReadLine()!);
 
-        Console.Write("Задайте высоту прямоугольника: ");
-        int b = int.Parse(Console.ReadLine());
+		Console.Write("Задайте высоту прямоугольника: ");
+		var b = int.Parse(Console.ReadLine()!);
 
-        for (int height = 1; height <= b; height++)
-        {
-            for (int width = 1; width <= a; width++)
-            {
-                if (width > 1 && width < a && height > 1 && height < b)
-                {
-                    Console.Write(" ");
-                }
-                else
-                {
-                    Console.Write("*");
-                }
-            }
-            Console.WriteLine();
-        }
-        Console.ReadLine();
-    }
+		for (var height = 1; height <= b; height++)
+		{
+			for (var width = 1; width <= a; width++)
+			{
+				if (width > 1 && width < a && height > 1 && height < b)
+				{
+					Console.Write(" ");
+				}
+				else
+				{
+					Console.Write("*");
+				}
+			}
+
+			Console.WriteLine();
+		}
+
+		Console.ReadLine();
+	}
 }
