ec46d9dc4005c2bd6192d86e4cd7ea0ec027ac5c
Vlad Moisak <pain1797@icloud.com>
2022-10-24 13:33:30 +0300

    Реализация: Rectangle is empty.

---
+++ Basics/Прямоугольник пустой/Program.cs
@@ -0,0 +1,28 @@
+﻿public static class Program
+{
+    public static void Main()
+    {
+        Console.Write("Задайте ширину прямоугольника: ");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.Write("Задайте высоту прямоугольника: ");
+        int b = int.Parse(Console.ReadLine());
+
+        for (int height = 1; height <= b; height++)
+        {
+            for (int width = 1; width <= a; width++)
+            {
+                if (width > 1 && width < a && height > 1 && height < b)
+                {
+                    Console.Write(" ");
+                }
+                else
+                {
+                    Console.Write("*");
+                }
+            }
+            Console.WriteLine();
+        }
+        Console.ReadLine();
+    }
+}
