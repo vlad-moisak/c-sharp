f7c56c0d47168619b4fd68edb35058086dc0f0d0
Vlad Moisak <pain1797@icloud.com>
2022-10-27 13:05:37 +0300

    Реализация: Triangle 2.

---
+++ Basics/Triangle 2/Program.cs
@@ -0,0 +1,18 @@
+﻿public static class Program
+{
+	public static void Main()
+	{
+        Console.Write("Задайте высоту треугольника: ");
+        int height = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = height; j > i; j--)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+        Console.ReadLine();
+    }
+}
