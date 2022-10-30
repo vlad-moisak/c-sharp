de20ff746e7b61d8513e4053732ca20f899d7ca7
Vlad Moisak <pain1797@icloud.com>
2022-10-30 11:32:37 +0200

    Реализация: Triangle 5.

---
+++ Basics/Triangle 5/Program.cs
@@ -0,0 +1,24 @@
+﻿public static class Program
+{
+	public static void Main()
+	{
+		Console.Write("Задайте высоту треугольника: ");
+		int height = int.Parse(Console.ReadLine());
+
+		int a = 0;
+
+		for (int i = height; i > 0; i--)
+		{
+			for (int j = 0; j < i; j++)
+			{
+				Console.Write(" ");
+			}
+			for (int f = 0; f <= a * 2; f++)
+			{
+				Console.Write("*");
+			}
+			Console.WriteLine();
+			a++;
+		}
+	}
+}
