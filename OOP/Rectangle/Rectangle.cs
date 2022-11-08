8dc6adbe1a6453f5dcb5bc88bcbe9c15ba668318
Tim Ivashchenko <tymofii.ivashchenko@gmail.com>
2022-11-08 14:54:04 +0200

    Класс Rectangle.

---
+++ OOP/Rectangle/Rectangle.cs
@@ -0,0 +1,41 @@
+﻿class Rectangle
+{
+	private int _a;
+	private int _b;
+
+	public Rectangle(int a, int b)
+	{
+		_a = a;
+		_b = b;
+	}
+
+	public int GetA()
+	{
+		return _a;
+	}
+
+	public int GetB()
+	{
+		return _b;
+	}
+
+	public int GetArea()
+	{
+		return _a * _b;
+	}
+
+	public int GetPerimeter()
+	{
+		return (_a * 2) + (_b * 2);
+	}
+
+	public void SetA(int a)
+	{
+		_a = a;
+	}
+
+	public void SetB(int b)
+	{
+		_b = b;
+	}
+}
