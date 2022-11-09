1c289b73b1789cd06b8aaa1fe70688fe9ac1a8ef
Tim Ivashchenko <tymofii.ivashchenko@gmail.com>
2022-11-09 13:53:53 +0200

    Пример юнит-тестирования.

---
+++ TDD/TDD Example/ConsoleApp.Tests/CalculatorTests.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+
+namespace ConsoleApp.Tests;
+
+public class CalculatorTests
+{
+	[Theory]
+	[InlineData(-1, -2)]
+	[InlineData(4, 2)]
+	[InlineData(7, 6)]
+	[InlineData(0, 0)]
+	[InlineData(0, -25)]
+	[InlineData(0, 50)]
+	public void Test_Sum(int a, int b)
+	{
+		// Arrange.
+
+		var calculator = new Calculator();
+
+		// Act.
+
+		var result = calculator.Sum(a, b);
+
+		// Assert.
+
+		Assert.Equal(a + b, result);
+	}
+}
---
+++ TDD/TDD Example/ConsoleApp/Calculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp;
+
+public class Calculator
+{
+	public int Sum(int a, int b)
+	{
+		return a + b;
+	}
+
+	public int Subtract(int a, int b)
+	{
+		throw new NotImplementedException();
+	}
+
+	public int Multiply(int a, int b)
+	{
+		throw new NotImplementedException();
+	}
+
+	public double Divide(int a, int b)
+	{
+		throw new NotImplementedException();
+	}
+}
