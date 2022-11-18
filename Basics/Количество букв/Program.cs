b8b423c91287a708c9e970863ae3e8b124dbacc8
Vlad Moisak <pain1797@icloud.com>
2022-11-18 10:31:03 +0200

    lettetsCount.

---
+++ Basics/Количество букв/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class Program
+{
+	public static void Main()
+	{
+		string text = "asdfga";
+
+		Dictionary<char, int> lettersCount = new Dictionary<char, int>();
+
+		for (int i = 0; i < text.Length; i++)	
+		{
+			char c = text[i];
+
+			if (!lettersCount.ContainsKey(c))
+			{
+				lettersCount.Add(c, 1);
+			}
+			else
+			{
+				lettersCount[c]++;
+			}
+		}
+
+		foreach (KeyValuePair<char, int> kvp in lettersCount)
+		{
+			Console.WriteLine("Key = {0}, Value ={1}", kvp.Key, kvp.Value);
+		}
+
+		Console.ReadLine();
+	}
+}
