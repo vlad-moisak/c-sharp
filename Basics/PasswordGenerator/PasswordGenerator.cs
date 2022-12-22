41891eacd3e9c8065b3e36b550d03e15d95527c0
Timofey Ivashchenko <tymofii.ivashchenko@gmail.com>
2022-12-22 12:22:01 +0200

    New version.

---
+++ Basics/PasswordGenerator/PasswordGenerator.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class PasswordGenerator
+{
+	private static readonly char[] LowerCaseLetters =
+	{
+		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+		'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+	};
+
+	private static readonly char[] Numbers =
+	{
+		'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
+	};
+
+	private static readonly char[] Symbols =
+	{
+		'@', '/', '!', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+',
+		'=', '[', ']', '{', '}', ':', ';', '\'', '`', '~', '|', ',', '<', '.',
+		'>', '?', '№'
+	};
+
+	private static readonly char[] UpperCaseLetters =
+	{
+		'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+		'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+	};
+
+	public string GeneratePassword(int passwordLength)
+	{
+		char[] characters = new char[passwordLength];
+		Random random = new Random();
+
+		for (int i = 0; i < passwordLength; i++)
+		{
+			char[] array = GetArray(i);
+			int randomIndex = random.Next(0, array.Length - 1);
+			characters[i] = array[randomIndex];
+		}
+
+		string password = string.Empty;
+		List<int> selectedIndexes = new List<int>();
+
+		while (true)
+		{
+			int index = random.Next(0, passwordLength);
+			if (selectedIndexes.Contains(index)) continue;
+			selectedIndexes.Add(index);
+			password += characters[index];
+			if (password.Length == passwordLength) break;
+		}
+
+		return password;
+	}
+
+	private static char[] GetArray(int index)
+	{
+		if (index % 4 == 0) return Numbers;
+		if (index % 4 == 1) return UpperCaseLetters;
+		if (index % 4 == 2) return LowerCaseLetters;
+		return Symbols;
+	}
+}
--- Basics/PasswordGenerator/Program.cs
+++ Basics/PasswordGenerator/Program.cs
@@ -1,106 +1,26 @@
 ﻿using System;
-using System.Linq;
 
 public static class Program
 {
 	public static void Main()
 	{
-		int? count;
+		int passwordLength = GetPasswordLength();
+		PasswordGenerator passwordGenerator = new PasswordGenerator();
+		string password = passwordGenerator.GeneratePassword(passwordLength);
+		Console.WriteLine($"Ваш пароль: {password}");
+	}
 
-		string password = "";
-
-		char[] rndPassword;
-
-		string[] numbers =
-		{
-			"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"
-		};
-		string[] word1 =
-		{
-			"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N",
-			"O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
-		};
-		string[] word2 =
-		{
-			"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
-			"m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
-		};
-		string[] symbols =
-		{
-			"@", "/", "!", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+",
-			"=", "[", "]", "{", "}", ":", ";", "'", "`", "~", "|", ",", "<", ".",
-			">", "?", "№"
-		};
-
-		Random rnd = new Random();
+	private static int GetPasswordLength()
+	{
+		int passwordLength;
 
 		do
 		{
-			Console.WriteLine("Пароль может быть от 8 до 36 символов!");
+			Console.WriteLine("Пароль должен быть от 8 до 32 символов.");
 			Console.Write("Введите длину пароля: ");
-			count = int.Parse(Console.ReadLine()!);
-		} while (count < 8 || count > 36);
+			passwordLength = int.Parse(Console.ReadLine()!);
+		} while (passwordLength is < 8 or > 32);
 
-		do
-		{
-			for (int i = 0; i < count / 4; i++)
-			{
-				password = password + numbers[rnd.Next(0, 9)] + word1[rnd.Next(0, 26)]
-									 + word2[rnd.Next(0, 26)] + symbols[rnd.Next(0, 31)];
-			}
-
-			String str = password;
-
-			for (int i = 0; i < numbers.Length; i++)
-			{
-				int index1 = str.IndexOf(numbers[i], StringComparison.Ordinal);
-				
-				if (index1 != -1)
-				{
-					break;
-				}
-			}
-
-			for (int i = 0; i < word1.Length; i++)
-			{
-				int index2 = str.IndexOf(word1[i], StringComparison.Ordinal);
-				
-				if (index2 != -1)
-				{
-					break;
-				}
-			}
-
-			for (int i = 0; i < word2.Length; i++)
-			{
-				int index3 = str.IndexOf(word2[i], StringComparison.Ordinal);
-				
-				if (index3 != -1)
-				{
-					break;
-				}
-			}
-
-			for (int i = 0; i < symbols.Length; i++)
-			{
-				int index4 = str.IndexOf(symbols[i], StringComparison.Ordinal);
-				
-				if (index4 != -1)
-				{
-					break;
-				}
-			}
-
-		} while (false);
-
-		rndPassword = password.ToCharArray();
-
-		int KeySelector(char x) => rnd.Next();
-
-		rndPassword = rndPassword.OrderBy(KeySelector).ToArray();
-		Console.Write("\n Ваш пароль: ");
-		Console.Write(string.Join("", rndPassword));
+		return passwordLength;
 	}
 }
-
-
